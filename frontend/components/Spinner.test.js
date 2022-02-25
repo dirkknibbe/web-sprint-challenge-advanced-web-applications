a7506c2aa7102e9aa69ee64a7c241dabcd8294f6
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-02-25 15:17:03 -0700

    ok i think i got a proper test for the spinner written

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,8 +1,14 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Spinner from "./Spinner";
 
 test("sanity", () => {
-  expect(true).toBe(true);
+  render(<Spinner on={true} />);
+
+  let spinnerText = screen.getByText("Please wait...");
+
+  expect(spinnerText).toBeInTheDocument();
 });
